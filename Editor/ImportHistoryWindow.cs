ee42b9d947a16f0f6b211b3f511772fd6f194456
Steffenvy <45214197+Steffenvy@users.noreply.github.com>
2020-06-03 00:21:34 +0200

    Update ImportHistoryWindow.cs

--- Editor/ImportHistoryWindow.cs
+++ Editor/ImportHistoryWindow.cs
@@ -7,6 +7,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Text;
 
 public class ImportHistoryPP : AssetPostprocessor
 {
@@ -14,45 +15,50 @@ public class ImportHistoryPP : AssetPostprocessor
 
     private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
     {
-        var windows = Resources.FindObjectsOfTypeAll<ImportHistoryWindow>();
-
-        for (int i = 0; i < windows.Length; i++)
+        //Ignores massive operations
+        const int MAX_COUNT = 300;
+        int count = deletedAssets.Length + movedAssets.Length + importedAssets.Length;
+        if (count > MAX_COUNT)
         {
-            var w = windows[i];
-
-            for (int ii = 0; ii < deletedAssets.Length; ii++)
-                w.history.Remove(deletedAssets[ii]);
-            for (int ii = 0; ii < movedFromAssetPaths.Length; ii++)
-                w.history.Remove(movedFromAssetPaths[ii]);
-
-            void Add(string path)
-            {
-                if (!ignores.Remove(path))
-                    w.Add(path);
-            }
-
-            for (int ii = 0; ii < movedAssets.Length; ii++)
-            {
-                var moved = movedAssets[ii];
-
-                bool imported = false; //Renaming an asset in Unity will call both movedAssets and importedAssets, but that creates a problem for the ignores
-                for (int iii = 0; iii < importedAssets.Length; iii++)
-                {
-                    if(importedAssets[iii] == moved)
-                    {
-                        imported = true;
-                        break;
-                    }
-                }
-
-                if(!imported)
-                    Add(moved);
-            }
-            for (int ii = 0; ii < importedAssets.Length; ii++)
-            {
-                Add(importedAssets[ii]);
-            }
+            ignores.Clear();
+            return;
         }
+
+
+        for (int ii = 0; ii < deletedAssets.Length; ii++)
+            ImportHistoryWindow.Remove(deletedAssets[ii]);
+        for (int ii = 0; ii < movedFromAssetPaths.Length; ii++)
+            ImportHistoryWindow.Remove(movedFromAssetPaths[ii]);
+
+        void Add(string path)
+        {
+            if (!ignores.Remove(path))
+                ImportHistoryWindow.Add(path);
+        }
+
+        for (int ii = 0; ii < movedAssets.Length; ii++)
+        {
+            var moved = movedAssets[ii];
+
+            bool imported = false; //Renaming an asset in Unity will call both movedAssets and importedAssets, but that creates a problem for the ignores
+            for (int iii = 0; iii < importedAssets.Length; iii++)
+            {
+                if (importedAssets[iii] == moved)
+                {
+                    imported = true;
+                    break;
+                }
+            }
+
+            if (!imported)
+                Add(moved);
+        }
+        for (int ii = 0; ii < importedAssets.Length; ii++)
+        {
+            Add(importedAssets[ii]);
+        }
+
+        ImportHistoryWindow.TryWrite();
     }
 }
 
@@ -112,7 +118,9 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
 
 
     //Fields
-    public List<string> history = new List<string>();
+    public static readonly List<string> history = new List<string>();
+    private static bool dirty;
+
     public Vector2 scrollPosition;
     private GUIStyle pingStyle, folderStyle;
     private Texture folderIcon;
@@ -137,10 +145,14 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
     private void Clear()
     {
         history.Clear();
+        dirty = true;
+        TryWrite();
     }
 
-    public void Add(string path)
+    public static void Add(string path)
     {
+        path = path.Replace("\\", "/");
+
         //Ignores by extension
         var lower = path.ToLowerInvariant();
         for (int i = 0; i < IGNORED_EXTENSIONS.Length; i++)
@@ -154,16 +166,58 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
         //Removes Duplicates
         for (int i = history.Count - 1; i >= 0; i--)
         {
-            if(history[i] == path)
+            if (history[i] == path)
                 history.RemoveAt(i);
         }
-        
+
         //Adds
         history.Insert(0, path);
 
         //Removes extra
         while (history.Count > HISTORY_LENGTH)
             history.RemoveAt(HISTORY_LENGTH);
+
+        dirty = true;
+    }
+    public static void Remove(string path)
+    {
+        path = path.Replace("\\", "/");
+
+        history.Remove(path);
+        dirty = true;
+    }
+
+    public static void TryWrite()
+    {
+        if(dirty)
+        {
+            dirty = false;
+
+            StringBuilder sb = new StringBuilder(1000);
+
+            for (int i = 0; i < history.Count; i++)
+            {
+                sb.Append(history[i]);
+                if (i != history.Count - 1)
+                    sb.Append("\\");
+            }
+
+            EditorPrefs.SetString("Import History", sb.ToString());
+        }
+    }
+    private static void Read()
+    {
+        history.Clear();
+
+        var s = EditorPrefs.GetString("Import History");
+
+        var paths = s.Split('\\');
+        for (int i = 0; i < paths.Length; i++)
+        {
+            var p = paths[i];
+            if(!string.IsNullOrEmpty(p))
+                history.Add(p);
+        }
     }
 
 
@@ -174,6 +228,9 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
         minSize = new Vector2(200, 50);
 
         folderIcon = Resources.Load<Texture>("IHW Folder Icon");
+
+        if (history.Count == 0)
+            Read();
     }
 
     public void OnGUI()
@@ -211,18 +268,7 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
             for (int i = 0; i < history.Count; i++)
             {
                 var path = history[i];
-
-
-                //Removes if can't be loaded
-                var asset = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object));
-                if (asset == null)
-                {
-                    history.RemoveAt(i);
-                    i--;
-                    continue;
-                }
-
-
+                
                 GUILayout.BeginHorizontal(GUILayout.Width(windowWidth));
                 {
                     //Ping Button
@@ -230,6 +276,16 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
 
                     if (GUILayout.Button(name, pingStyle, pingWidth, height))
                     {
+                        //Removes if can't be loaded
+                        var asset = AssetDatabase.LoadAssetAtPath(path, typeof(UnityEngine.Object));
+                        if (asset == null)
+                        {
+                            history.RemoveAt(i);
+                            i--;
+                            dirty = true;
+                            continue;
+                        }
+
                         //Pings and selects
                         EditorGUIUtility.PingObject(asset);
                         Selection.activeObject = asset;
@@ -262,5 +318,8 @@ public class ImportHistoryWindow : EditorWindow, IHasCustomMenu
             }
         }
         EditorGUILayout.EndScrollView();
+
+
+        TryWrite();
     }
 }
